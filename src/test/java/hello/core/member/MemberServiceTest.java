bde226fae7b73f07927ceddd9a92d365f9bad202
liveforone <yc4852@gmail.com>
2022-02-06 19:47:52 +0900

    good commit

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,11 +1,19 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
+
+    @BeforeEach  //이 구문이 먼저 실행되고 아래 테스트가 돌아감
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();  //AppConfig를 만듦
+        memberService = appConfig.memberService();  //멤버 서비스를 위의 객체에 할당해준 후에 아래 테스트가 실행
+    }
 
     @Test
     void join() {
--- src/test/java/hello/core/order/OrderServiceTest.java
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -1,16 +1,25 @@
 package hello.core.order;
 
+import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class OrderServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
-    OrderService orderService = new OrderServiceImpl();
+    MemberService memberService;
+    OrderService orderService;
+
+    @BeforeEach  //이 구문이 먼저 실행되고 아래 테스트가 돌아감
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();  //AppConfig를 만듦
+        memberService = appConfig.memberService();  //멤버 서비스를 위의 객체에 할당해준 후에 아래 테스트가 실행
+        orderService = appConfig.orderService();
+    }
 
     @Test
     void createOrder() {
