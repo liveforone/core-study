d91c0a2de7dcfb6982c10e164c30d1d43c6ea083
liveforone <yc4852@gmail.com>
2022-02-09 23:09:39 +0900

    good commit

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -15,18 +15,35 @@ import org.springframework.context.annotation.Configuration;
 @Configuration  //구성하는 클래스에는 해당 어노테이션을 붙여주어야한다.
 public class AppConfig {
 
+    //@Bean -> memberService -> new MemoryMemberRepository()
+    //@Bean -> orderService -> new MemoryMemberRepository()
+
+    //call AppConfig.memberService
+    //call AppConfig.memberRepository
+    //call AppConfig.memberRepository
+    //call AppConfig.orderService
+    //call AppConfig.memberRepository
+
+    //예상과 다르게 결과는 아래와 같다. 즉, 스프링이 싱글톤을 지켰다.
+    //call AppConfig.memberService
+    //call AppConfig.memberRepository
+    //call AppConfig.orderService
+
     @Bean //메서드이름이 스프링 컨테이너에 등록됨
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }  //서비스 구현
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }  //저장소 구현
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }  //오더서비스는 멤버저장소를 가져오고 할인정책불러오기
 
