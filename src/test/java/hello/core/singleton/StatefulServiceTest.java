3a75e525a1941328f206bb1ee33861d067ffe04a
liveforone <yc4852@gmail.com>
2022-02-09 21:37:46 +0900

    good commit

---
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,37 @@
+package hello.core.singleton;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatefulServiceTest {
+
+    @Test
+    void statefulServiceSingleton() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+        //쓰레드가 위에서 아래 순서로 진행됨, 즉 A사용자가 주문 -> B사용자가 주문 -> A사용자 조회
+        //ThreadA : A사용자 10000원 주문
+        statefulService1.order("userA", 10000);
+        //ThreadB : B사용자 20000원 주문
+        statefulService2.order("userB", 20000);
+
+        //ThreadA : 사용자A 주문 금액 조회
+        int price = statefulService1.getPrice();
+        System.out.println("price = " + price);  //A사용자의 금액을 조회했지만 B사용자의 금액이 조회되었다. 즉 실패
+        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+    }
+
+    static class TestConfig {
+
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+    }
+}
