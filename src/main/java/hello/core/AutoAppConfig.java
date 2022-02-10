4f77f39af2f2453904e200023a4b0ce921379ea7
liveforone <yc4852@gmail.com>
2022-02-10 12:07:41 +0900

    good commit

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,16 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+/*
+컴포넌트 스캔을 해서 @Configuration클래스는 필터로 빼줄것이다.
+왜냐하면 AppConfig클래스는 수동 등록한 것이라서 현재 클래스와 충둘이 나면 안되기 때문이다.
+ */
+public class AutoAppConfig {
+}
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -4,12 +4,16 @@ import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;  //회원 찾기
     private final DiscountPolicy discountPolicy;  //인터페이스에만 의존하게 됨
 
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
